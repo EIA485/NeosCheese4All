cb760854df250abea995862ba4b6caa8c0c4bfea
eia485 <eia485git@gmail.com>
2021-08-27 07:57:53 -0700

    Add project files.

---
+++ Cheese4All/Cheese4All.cs
@@ -0,0 +1,39 @@
+﻿using HarmonyLib;
+using NeosModLoader;
+using FrooxEngine;
+using BaseX;
+using System;
+using CloudX.Shared;
+
+namespace Cheese4All
+{
+    public class Cheese4All : NeosMod
+    {
+        public override string Name => "Cheese4All";
+        public override string Author => "eia485";
+        public override string Version => "1.0.0";
+        public override string Link => "https://github.com/EIA485/NeosCheese4All/";
+        public override void OnEngineInit()
+        {
+            Harmony harmony = new Harmony("net.eia485.Cheese4All");
+            harmony.PatchAll();
+        }
+        [HarmonyPatch(typeof(PointerInteractionController), "OnCommonUpdate")]
+        class Cheese4AllPatch
+        {
+            public static void Prefix(ref bool ____cheeseActivated, ref UnlitMaterial ____pointerMaterial, PointerInteractionController __instance)
+            {
+                if (__instance.World == Userspace.UserspaceWorld&&!____cheeseActivated);
+                {
+                    ____cheeseActivated = true;
+                    ____pointerMaterial.TintColor.Value = new color(1,1,1,1);
+                    //implement the ultra top secret (so secret froox doesn't even know about it) potato mode
+                    if (__instance.Cloud.CurrentUser?.Id == "U-ProbablePrime")
+                        ____pointerMaterial.Texture.Target = (IAssetProvider<ITexture2D>)__instance.Slot.AttachTexture(NeosAssets.Graphics.Badges.potato);
+                    else//enable cheese for the masses
+                        ____pointerMaterial.Texture.Target = (IAssetProvider<ITexture2D>)__instance.Slot.AttachTexture(NeosAssets.Graphics.Badges.Cheese);
+                }
+            }
+        }
+    }
+}
