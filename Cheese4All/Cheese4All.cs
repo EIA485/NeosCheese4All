5cc55c7ff731e6584df3d5b1dcfb373d9c8a09c2
EIA485 <17285570+EIA485@users.noreply.github.com>
2022-07-30 05:48:47 -0700

    forgot to push code

--- Cheese4All/Cheese4All.cs
+++ Cheese4All/Cheese4All.cs
@@ -11,13 +11,19 @@ namespace Cheese4All
     {
         public override string Name => "Cheese4All";
         public override string Author => "eia485";
-        public override string Version => "1.1.0";
+        public override string Version => "1.2.0";
         public override string Link => "https://github.com/EIA485/NeosCheese4All/";
+        private static ModConfiguration config;
+
+        static ModConfigurationKey<bool> AccurateCheese = new("AccurateCheese", "use unadulterated cheese", () => false);
+
         public override void OnEngineInit()
         {
+            config = GetConfiguration();
             Harmony harmony = new Harmony("net.eia485.Cheese4All");
             harmony.PatchAll();
         }
+
         [HarmonyPatch(typeof(PointerInteractionController), "BeforeInputUpdate")]
         class Cheese4AllPatch
         {
@@ -32,14 +38,17 @@ namespace Cheese4All
                         ____pointerMaterial.Texture.Target = (IAssetProvider<ITexture2D>)__instance.Slot.AttachTexture(NeosAssets.Graphics.Badges.potato);
                     else//enable cheese for the masses
                         ____pointerMaterial.Texture.Target = (IAssetProvider<ITexture2D>)__instance.Slot.AttachTexture(NeosAssets.Graphics.Badges.Cheese);
-                    
-                    StaticTexture2D tex = ____pointerMaterial.Texture.Target as StaticTexture2D;
-                    tex.FilterMode.Value = TextureFilterMode.Anisotropic;
-                    tex.Uncompressed.Value = true;
-                    tex.WrapModeU.Value = TextureWrapMode.Clamp;
-                    tex.WrapModeV.Value = TextureWrapMode.Clamp;
-                    tex.PowerOfTwoAlignThreshold.Value = 1;
-                    tex.MipMapFilter.Value = CodeX.Filtering.Bilinear;
+
+                    if (!config.GetValue(AccurateCheese))
+                    {
+                        StaticTexture2D tex = ____pointerMaterial.Texture.Target as StaticTexture2D;
+                        tex.FilterMode.Value = TextureFilterMode.Anisotropic;
+                        tex.Uncompressed.Value = true;
+                        tex.WrapModeU.Value = TextureWrapMode.Clamp;
+                        tex.WrapModeV.Value = TextureWrapMode.Clamp;
+                        tex.PowerOfTwoAlignThreshold.Value = 1;
+                        tex.MipMapFilter.Value = CodeX.Filtering.Bilinear;
+                    }
 
                 }
             }
